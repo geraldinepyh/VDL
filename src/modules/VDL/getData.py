00cb4663b055462f6137c6a8b3f56e4eabec1f54
Geraldine <geraldinepyh@gmail.com>
2019-10-11 19:18:21 +0800

    updated querier to pull by chunks

--- src/modules/VDL/getData.py
+++ src/modules/VDL/getData.py
@@ -8,120 +8,107 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-class Querier():
+config = jsonref.load(open('../config/config.json'))
+logBase = config['logging']['logBase'] + '.modules.VDL.getData'
 
-    def __init__(self, name, fpath='../data/'):
-        self.name = name
-        self.fpath = fpath
-        self.config = jsonref.load(open('../config/config.json'))
-        self.logBase = config['logging']['logBase'] + '.modules.VDL.getData'
-        self.projConfig = jsonref.load(open('../config/modules/getData.json'))
-        self.data = pd.DataFrame()
-    
-    def saveData(self, savePath, override=False):
+class Database:
+   '''Database static (stateful) class
+   Used to manage database session details and contains common database
+   pulling code.
+   Variables:
+       dbName {string} -- Name of the database
+       dbVersion {string} -- Operating schema (representing the version
+                             of database) to be used
+   '''
+   projConfig = jsonref.load(open('../config/modules/getData.json'))
+   dbName    = projConfig['inputs']['dbName']
+   dbVersion = projConfig['inputs']['dbVersion']
 
-        saveFile = f'{savePath}/{self.name}.pkl'
-        if not os.path.exists(saveFile) or override:
+   def PullData(self, query, columns, saveData=True, savePath='../data/intermediate', saveName='temp'):
+        '''Function for pulling from the database
+        [description]
+        Arguments:
+            query {string} -- Postgre query to be sent to the server.
+            columns {list} -- List of strings corresponding to the column
+                                names.
+        Returns:
+            Output -- Returns output data.
+        '''
+        # Iterator for query
+        dataIterator = pgIO.getDataIterator(query, dbName = Database.dbName, chunks=1000)
+        # Run through iterator, appending to buffer
+        dataBuffer = []
+        for data in dataIterator:
+            dataBuffer += data
+        # Convert data in bufer to dataframe
+        dataOut = pd.DataFrame(dataBuffer, columns=columns)
+
+        if saveData:
             if not os.path.exists(savePath):
                 os.makedirs(savePath)
-            df.to_pickle(saveFile)
-            print(saveFile, 'saved.')
-        else:
-            print('File already exists. Please specify if you want to override.')
+            dataOut.to_pickle(os.path.join(savePath,saveName+'.pkl'))
+            print(saveName+'.pkl saved.')
 
-    def getData(self, query, cols=[], saveData=True):
+        return dataOut
 
-        dbName = self.projConfig['inputs']['dbName']
-        dbVersion = self.projConfig['inputs']['dbVersion']
-        
-        data = pgIO.getAllData(query, dbName=dbName)
-        df = pd.DataFrame(data)
-        if cols != []: df.columns = cols
-        if saveData:
-            self.saveData(self.fpath, override=True)
-        return df
-    
-    def filterData(self, filterPath):
-        # Assuming filter csv has 2 columns only. 
-        # e.g. (Actual_Values, Recoded_Values)
-
-        data_filter = pd.read_csv(filterPath, header=None)
-        data_filter_query = tuple([mdd for mdd in data_filter[0]])
-
-        return data_filter_query
-
-@lD.log(logBase + '.prepData')
-def prepData(logger):
-    mdd_q = Querier(mdd_patients, '../data/temp')
-    # full_q = Querier(full_data, '../data/temp')
-
-    # 'Filter' to select my patients of interest. (MDD Patients) 
-    # Some other way to provide a list of patientIDs for patients i'm interested in? 
-    mdd_filter_query = mdd_q.filterData('../data/raw_data/Filters/data_Filter.csv')
-    mdd_query = f"""select
-                        distinct pd.patientid
-                    from
-                        rwe_version1_2.pdiagnose pd
-                    inner join (
-                        select
-                            tp.patientid, meds.medication,min(tp.days),
-                            max(tp.days), count(distinct tp.typepatientid)
-                        from
-                            rwe_version1_2.typepatient as tp,
-                            rwe_version1_2.meds as meds
-                        where
-                            tp.typepatientid = meds.typepatientid
-                        group by
-                            tp.patientid,
-                            meds.medication
-                        having
-                            max(tp.days) - min(tp.days) >= 120
-                            and count(distinct tp.typepatientid) >= 3 ) pts on
-                        pts.patientid = pd.patientid
-                    where
-                        cast(pd.dsmno as text) in {mdd_filter_query}
-                        and pd.primary_dx = true ; """
-
-    mdd_patients = mdd_q.getData(mdd_query, cols=['PatientID'], savePath='intermediate/mdd_patients')
-    mdd_pts_list = tuple([pt for pt in mdd_patients['PatientID']])
-
-    queries = {
-        "typepatient" : ["patientid", "typepatientid", "visit_name", "visit_type", "age", "days"]
-        "background" : ["patientid", "sex", "race"],
-        "pdiagnose" : ["patientid", "typepatientid", "diagnosis", "dsmno"],
-        "medication" : ["patientid", "typepatientid", "medication", "dose"],
-        "cgi" : ["patientid", "typepatientid", "severity"]
-    }
-
-    results = {}
-    count = 0
-    for tbl, cols in queries.items():
-        print(tbl)
-        results["result"+str(count)] = Querier(tbl, f'../data/temp/{tbl}.pkl')
-        data_query = f"""select distinct on ({tbl}.patientid), {col for col in cols}
-                        from rwe_version1_2.{tbl}
-                        where {tbl}.patientid in {mdd_pts_list}
-                        """
-        count+=1
-    print(results)
+@lD.log(logBase + '.testQuery')
+def testQuery(logger):
+    q = Database()
+    query = f'''
+        select pd.patientid, pd.typepatientid, pd.diagnosis
+        from rwe_version1_2.pdiagnose pd
+        where patientid = 9 
+        limit 20
+    '''
+    data = q.PullData(query, ['PatientID','VisitID', 'Diagnosis'], saveData=False)
+    print(data.head())
+    print('Data was pulled successfully.')
     
     return 
 
-@lD.log(logBase + '.getComparativePop')
-def getComparativePop(logger, patientID):
-    data = prepData()
 
-    return patientData, popData
+
+@lD.log(logBase + '.prepData')
+def prepData(logger):
+    q = Database()
+    cohort_dsm = ('296.20', '296.21',     '296.22',    '296.23',    '296.24',    '296.25',
+                '296.26',    'F32.0',    'F32.1',    'F32.2',    'F32.4',
+                'F32.5',    'F32.81',    'F32.89',    '296.30',    '296.31',
+                '296.32',    '296.33',    '296.34', '296.35',    '296.36',
+                'F33.0',    'F33.1',    'F33.2',    'F33.40',    'F33.41',
+                'F33.42',    'F33.8',    '311',    'F32.9',    'F33.9',
+                '300.4',    'F34.1')
+    cohort_query = f"""select
+        distinct pd.patientid, pd.typepatientid, tp.days
+    from
+        rwe_version1_2.pdiagnose pd, rwe_version1_2.typepatient as tp
+    where pd.patientid in (
+        select
+            tp.patientid
+        from
+            rwe_version1_2.typepatient as tp,
+            rwe_version1_2.meds as meds
+        where
+            tp.typepatientid = meds.typepatientid
+        group by
+            tp.patientid,
+            meds.medication
+        having
+            max(tp.days) - min(tp.days) >= 120
+            and count(distinct tp.typepatientid) >= 3 )
+    and
+        cast(pd.dsmno as text) in {cohort_dsm}
+        and pd.primary_dx = true
+        """
+    data = q.PullData(cohort_query, 
+                    ['PatientID', 'VisitID', 'Days'],
+                    saveName='mdd_visitsdays')
+    data.head()
+    return 
 
 
 @lD.log(logBase + '.main')
 def main(logger, resultsDict):
     print('Starting..')
-
+    prepData()
     print('Done!')
-
-    # ## Stash
-    # @interact
-    # def show_meds_CGIbyDays(med=meds_list, x=365):
-    #     temp = pop_mdd.loc[pop_mdd.Medication == med].groupby('Days').agg({'CGI-S':'median'})
-    #     return temp.loc[temp.index<x]
